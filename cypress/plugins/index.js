eca42828c9b4ef781169c9d593ea13b09a48ed24
mnarin <mbarkin.narin@gmail.com>
2021-02-19 14:17:29 +0300

    timeout update

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -43,7 +43,8 @@ module.exports = (on, config) => {
             const composeFile = "docker-compose.yml";
             const cont = await new DockerComposeEnvironment(composeFilePath, composeFile)
                 .withEnv("WORKER_IP", "10.150.17.73")
-                .withWaitStrategy("calculator-ui", Wait.forLogMessage("You can now view calculator-ui in the browser."))
+                .withStartupTimeout(120000)
+              //  .withWaitStrategy("calculator-ui", Wait.forLogMessage("You can now view calculator-ui in the browser."))
                 .up();
             // const props = {}
             // props.port = cont.getMappedPort(8070);
