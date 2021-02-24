1f0acced23c93620aa6d465d328bbca2caed220e
nozdemir <nozdemir.biz@gmail.com>
2021-02-24 09:08:09 +0300

    buttons accessing are revised to select accessing

--- cypress/integration/common/angry_step_definitions.js
+++ cypress/integration/common/angry_step_definitions.js
@@ -4,21 +4,21 @@ import calculatorConfigResolver from '../../config/config.json'
 
 Given(/^Either of any value is not given$/, () => {
 
-    cy.get('input[id="'+ calculatorConfigResolver.firstValueElementId + '"]')
+    cy.get('#' + calculatorConfigResolver.firstValueElementId)
         .type("5"); // only firstValue is set
 });
 
 When(/^Necati wants to perform (\S+) operation$/, (operation) => {
-    cy.get(calculatorConfigResolver.calculationSelectElementId)
-        .select(operation);
+    cy.get('#' + calculatorConfigResolver.calculationSelectElementId)
+        .type(operation + '{enter}')
 
-    cy.get('button[id="' + calculatorConfigResolver.calculationButtonElementId + '"]')
+    cy.get('#' + calculatorConfigResolver.calculationButtonElementId)
         .click();
 });
 
 Then(/^Necati should see a warning notification$/, () => {
-    cy.get('input[id="'+ calculatorConfigResolver.resultValueElementId + '"]')
+    cy.get('#' + calculatorConfigResolver.resultValueElementId)
         .should('have.value', '0');
-    cy.get('input[id="'+ calculatorConfigResolver.resultMessageElementId + '"]')
+    cy.get('#' + calculatorConfigResolver.resultMessageElementId)
         .should('have.value', 'FAIL');
 });
